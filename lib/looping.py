c33b6d36067b2ab270d7f6fd2085883a0e8b3936
Gi-thinji <githinji.ngare@gmail.com>
2023-08-17 00:52:48 +0300

    passed test

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,34 @@
 
 def happy_new_year():
     # code goes here!
+    i=10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
     pass
 
 def square_integers(int_list):
     # code goes here!
+    squared_list = []
+    for num in int_list:
+        squared_int =  num ** 2
+        squared_list.append(squared_int)
+    return squared_list
+
     pass
 
 def fizzbuzz():
     # code goes here!
+    i=1
+    while i<=100:
+        if i % 3 == 0 and i%5 ==0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print ("Buzz")
+        else:
+            print (i)
+        i+=1
     pass
